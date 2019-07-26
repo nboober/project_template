758800f308cf0619a66e0ae5db2475c7959a52ff
nboober <nboober@terpmail.umd.edu>
2019-07-26 16:27:36 -0400

    USer info now prints out the assigned roles. Also an issue was fixed where a new user was created after updating an old user

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -3,14 +3,20 @@ package com.example.demo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.naming.Binding;
+import javax.validation.Valid;
 
 @Controller
 public class HomeController {
 
     @Autowired
     UserRepository userRepository;
+
+    @Autowired
+    RoleRepository roleRepository;
 
     @Autowired
     private UserService userService;
@@ -21,7 +27,35 @@ public class HomeController {
         return "index";
     }
 
-//    @RequestMapping("/update/{id}")
+    @RequestMapping("/update/{id}")
+    public String updateUser(@PathVariable("id") long id, Model model){
+        model.addAttribute("roles", roleRepository.findAll());
+        model.addAttribute("user", userRepository.findById(id).get());
+        return "updateUser";
+    }
+
+    @PostMapping("/process")
+    public String processUpdates(@Valid @ModelAttribute("user") User user, BindingResult result, Model model){
+
+        model.addAttribute("user", user);
+        if(result.hasErrors()){
+            return "updateUser";
+        }
+
+        userService.saveUser(user);
+        return "redirect:/";
+    }
+
+    @RequestMapping("/delete/{id}")
+    public String deleteUser(@PathVariable("id") long id, Model model){
+        userRepository.deleteById(id);
+        model.addAttribute("users", userRepository.findAll());
+        model.addAttribute("roles", roleRepository.findAll());
+        return "admin";
+    }
+
+
+
 
 
 
